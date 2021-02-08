29cdd70866798be86bf6624128b45d2faf88932e
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-02-09 00:23:51 +0600

    Done

---
+++ app.js
@@ -0,0 +1,64 @@
+// Function : To search users...
+const searchSongs = async () => {
+    const searchText = document.getElementById("inputValue").value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
+        displaySongs(data.data);
+    }
+    catch{
+        displayError("Something went wrong! Please, check your spell & try again later.")
+    }
+}
+
+// Function : To display songs by searching...
+const displaySongs = songs => {
+    const songContainer = document.getElementById("songContainer");
+    songContainer.innerHTML = '';
+    document.getElementById("inputValue").value = '';
+    document.getElementById("showError").innerText = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = "single-result row align-items-center my-3 p-3"
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getSongLyrics('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `
+        songContainer.appendChild(songDiv);
+    });
+}
+
+// Function : To get the lyrics API...
+const getSongLyrics = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
+        displayLyrics(data.lyrics)
+    }
+    catch(error){
+        displayError('Sorry! I failed to load your lyrics.')
+    }
+}
+// Function : To display song lyrics...
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById("lyricsDiv")
+    lyricsDiv.innerText = lyrics;
+}
+
+// Function : To display error message...
+const displayError = error =>{
+    document.getElementById("songContainer").innerHTML = '';
+    document.getElementById("inputValue").value = '';
+    const errorTag = document.getElementById('showError');
+    errorTag.innerText = error;
+}
